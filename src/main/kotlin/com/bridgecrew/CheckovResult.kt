b8727811ba14ced6af56fb90b5fab12bfeb6e907
Nir Adler <nadler@paloaltonetworks.com>
2023-05-29 12:32:43 +0300

    risk factor

--- src/main/kotlin/com/bridgecrew/CheckovResult.kt
+++ src/main/kotlin/com/bridgecrew/CheckovResult.kt
@@ -1,8 +1,27 @@
 package com.bridgecrew
 
 import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 
 val gson = Gson()
+
+//data class RiskFactors(
+//        @SerializedName("Has fix")
+//        val has_fix: String?,
+//
+//        @SerializedName("Critical severity")
+//        val critical_severity: String?,
+//
+//        @SerializedName("DoS - High")
+//        val dos_: String?,
+//
+//        @SerializedName("Attack vector: network")
+//        val has_fix: String?,
+//
+//        @SerializedName("Attack complexity: low")
+//        val has_fix: String?,
+//)
 
 data class VulnerabilityDetails(
         val id: String?,
@@ -15,7 +34,7 @@ data class VulnerabilityDetails(
         val lowest_fixed_version: String?,
         val published_date: String?,
         val vector: String?,
-//        val risk_factors: List<String>?, // TODO - fix after Saar's team fixes their side
+        val risk_factors: Map<String, Any>,
         val root_package_name: String?,
         val root_package_version: String?,
         val root_package_fix_version: String?
