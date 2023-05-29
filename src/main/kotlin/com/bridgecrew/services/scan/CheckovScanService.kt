f39efa643c41442c3e4f76b9b99bf84da3884230
Nir Adler <nadler@paloaltonetworks.com>
2023-05-29 18:01:35 +0300

    action

--- src/main/kotlin/com/bridgecrew/services/scan/CheckovScanService.kt
+++ src/main/kotlin/com/bridgecrew/services/scan/CheckovScanService.kt
@@ -149,7 +149,7 @@ class CheckovScanService: Disposable {
     }
 
     private fun generateCheckovCommand(execCommand: List<String>): GeneralCommandLine {
-        val pluginVersion = PluginManagerCore.getPlugin(PluginId.getId("com.github.bridgecrewio.checkov"))?.version
+        val pluginVersion = PluginManagerCore.getPlugin(PluginId.getId("com.github.bridgecrewio.prismacloud"))?.version
                 ?: "UNKNOWN"
         val prismaUrl = settings?.prismaURL
 
