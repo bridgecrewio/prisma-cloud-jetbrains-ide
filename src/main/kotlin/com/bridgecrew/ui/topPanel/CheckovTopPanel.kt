d0ba5098abd607197639e49d552ed39ae797c298
sschweid <sschweid@paloaltonetworks.com>
2023-06-05 15:59:17 +0300

    more ui fixes

--- src/main/kotlin/com/bridgecrew/ui/topPanel/CheckovTopPanel.kt
+++ src/main/kotlin/com/bridgecrew/ui/topPanel/CheckovTopPanel.kt
@@ -1,28 +1,14 @@
 package com.bridgecrew.ui.topPanel
 
-import com.bridgecrew.analytics.AnalyticsService
-import com.bridgecrew.results.Category
 import com.bridgecrew.results.Severity
-import com.bridgecrew.services.CheckovResultsListUtils
-import com.bridgecrew.services.ResultsCacheService
-import com.bridgecrew.services.scan.FullScanStateService
 import com.bridgecrew.ui.buttons.SeverityFilterButton
 import com.bridgecrew.utils.*
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import java.awt.*
-import java.time.Duration
-import java.time.Instant
 import javax.swing.*
 import javax.swing.plaf.basic.BasicSeparatorUI
-
-data class ScanResultMetadata(
-        val totalIssues: Int,
-        val totalPassed: Int,
-        val scanDuration: Long
-)
 
 class CheckovTopPanel(val project: Project) : SimpleToolWindowPanel(true, true), Disposable {
 
@@ -44,12 +30,12 @@ class CheckovTopPanel(val project: Project) : SimpleToolWindowPanel(true, true),
 
     private fun addSeverityLabel(actionToolbarPanel: JPanel) {
         actionToolbarPanel.add(JLabel("Severity:"))
-        actionToolbarPanel.add(Box.createRigidArea(Dimension(5, 24)))
+        actionToolbarPanel.add(Box.createRigidArea(Dimension(5, 20)))
     }
 
     private fun createSeparator(actionToolbarPanel: JPanel) {
         val separator = JSeparator(JSeparator.VERTICAL)
-        separator.preferredSize = Dimension(2, 24)
+        separator.preferredSize = Dimension(2, 20)
         separator.setUI(object : BasicSeparatorUI() {
             override fun paint(g: Graphics, c: JComponent) {
                 val g2d = g.create() as Graphics2D
@@ -59,14 +45,18 @@ class CheckovTopPanel(val project: Project) : SimpleToolWindowPanel(true, true),
             }
         })
         actionToolbarPanel.add(separator)
-        actionToolbarPanel.add(Box.createRigidArea(Dimension(5, 24)))
+        actionToolbarPanel.add(Box.createRigidArea(Dimension(5, 20)))
     }
 
     private fun addFilterActions(actionToolbarPanel: JPanel) {
         actionToolbarPanel.add(SeverityFilterButton(project, "I", Severity.INFO))
+        actionToolbarPanel.add(Box.createRigidArea(Dimension(3, 0)))
         actionToolbarPanel.add(SeverityFilterButton(project,"L", Severity.LOW))
+        actionToolbarPanel.add(Box.createRigidArea(Dimension(3, 0)))
         actionToolbarPanel.add(SeverityFilterButton(project,"M", Severity.MEDIUM))
+        actionToolbarPanel.add(Box.createRigidArea(Dimension(3, 0)))
         actionToolbarPanel.add(SeverityFilterButton(project,"H", Severity.HIGH))
+        actionToolbarPanel.add(Box.createRigidArea(Dimension(3, 0)))
         actionToolbarPanel.add(SeverityFilterButton(project,"C", Severity.CRITICAL))
     }
 
