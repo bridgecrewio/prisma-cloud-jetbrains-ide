247e0e07d00d0647991cbea6d042cb8bf65aabe3
sschweid <sschweid@paloaltonetworks.com>
2023-06-08 12:04:31 +0300

    fix risk factors

--- src/main/kotlin/com/bridgecrew/initialization/InitializationService.kt
+++ src/main/kotlin/com/bridgecrew/initialization/InitializationService.kt
@@ -25,7 +25,7 @@ private val LOG = logger<InitializationService>()
 class InitializationService(private val project: Project) {
 
     private var isCheckovInstalledGlobally: Boolean = false
-    private var checkovVersion: String = "2.3.193"
+    private var checkovVersion: String = "2.3.253"
 
     fun initializeProject() {
         initializeCheckovScanService()
